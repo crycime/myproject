7d80673354b013fb0986cc52e4597ff13ad29598
zizhenli <snile657@outlook.com>
2023-03-30 20:15:17 +0700

    feat: init

---
+++ package1/index.js
@@ -0,0 +1,4 @@
+// package1/index.js
+module.exports = function() {
+    console.log('This is Package 1.');
+};
---
+++ package2/index.js
@@ -0,0 +1,4 @@
+// package2/index.js
+module.exports = function() {
+    console.log('This is Package 2.');
+};
